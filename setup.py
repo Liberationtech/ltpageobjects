4a0f72cd41f294827735636eebea21add679517e
Oivvio Polite <oivvio@liberationtech.net>
2014-08-13 14:03:28 +0200

    adding classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from distutils.core import setup
 
-# Requirements
-install_requires = [
-    'selenium',
-]
 
 setup(name='ltpageobjects',
-      version='0.1',
-      description='Liberationtech Page Objects',
-      author='Oivvio Polite',
-      author_email='oivvio@liberationtech.net',
-      url='https://github.com/Liberationtech/ltpageobjects',
-      install_requires=install_requires,
-      packages=['ltpageobjects'],
-     )
+        version='0.1',
+        description='Liberationtech Page Objects',
+        author='Oivvio Polite',
+        author_email='oivvio@liberationtech.net',
+        url='https://github.com/Liberationtech/ltpageobjects',
+        install_requires=['selenium'],
+        packages=['ltpageobjects'],
+        license="MIT",
+
+        classifiers=(
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT Licenser',
+            'Programming Language :: Python',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            )
+        )
