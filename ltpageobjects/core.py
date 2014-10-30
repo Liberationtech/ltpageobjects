fb407bd6321015bfe5eb2d173240f5d5b5bcb691
Oivvio Polite <oivvio@liberationtech.net>
2014-10-30 09:47:00 +0100

    this and that

--- ltpageobjects/core.py
+++ ltpageobjects/core.py
@@ -1,5 +1,12 @@
 from selenium.webdriver.support import expected_conditions as EC  # available since 2.26.0
 from selenium.webdriver.support.ui import WebDriverWait  # available since 2.4.0
+from selenium.webdriver.support.ui import Select
+import re
+
+#TODO Move to incorporating urls.py from current project
+# pick up url NAMES and also use the pattern matching from there
+# on top of that allow users to define new sites using the same mechanism
+
 
 class PageNotFoundException(Exception):
     pass
@@ -29,17 +36,20 @@ class PageObject:
         self.driver = driver
         self.site = site
 
-        if not self.__class__.base_url:
-            raise MissingBaseUrlException({"message":"You must supply a base_url class attribute when inheriting from PageObject"})
 
-        if not self.__class__.url:
-            raise MissingUrlException({"message":"You must supply a url class attribute when inheriting from PageObject"})
+        if self.__class__.base_url and self.__class__.url:
+            self.full_url = self.__class__.base_url + self.__class__.url
+        #if not self.__class__.base_url:
+        #    raise MissingBaseUrlException({"message":"You must supply a base_url class attribute when inheriting from PageObject"})
+
+        #if not self.__class__.url:
+        #    raise MissingUrlException({"message":"You must supply a url class attribute when inheriting from PageObject"})
 
     def get(self):
         print "self.url: {}".format(self.url)
-        print "self.full_url: {}".format(self.full_url())
+        print "self.full_url: {}".format(self.full_url)
         #self.driver.get(self.url)
-        self.driver.get(self.full_url())
+        self.driver.get(self.full_url)
 
     def wait(self, locator, timeout=10000):
         WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located(locator))
@@ -51,8 +61,6 @@ class PageObject:
         fh.write(self.driver.page_source.encode("UTF-8"))
         fh.close()
 
-    def full_url(self):
-        return self.__class__.base_url + self.__class__.url
 
     def find_element_by_link(self,link):
         return self.find_elements_by_xpath("//a[@href='{0}']".format(link))[0]
@@ -82,7 +90,27 @@ class PageObject:
             element.clear()
         element.send_keys(value)
 
-    def click(self, link, url_pattern=None):
+    def select_by_visible_text(self, field, text):
+        """
+        field is a callable on our PageObject that returns a field
+        text is the
+
+        """
+        select = Select(field())
+        select.select_by_visible_text(text)
+
+
+    def select_by_value(self, field, value):
+        """
+        field is a callable on our PageObject that returns a field
+        text is the
+
+        """
+        select = Select(field())
+        select.select_by_value(value)
+
+
+    def click(self, link):
         """
         link is a bound callable that returns an element
         click a link and return a new page object
@@ -93,14 +121,14 @@ class PageObject:
         link().click()
         return self.site.newpage(self)
 
-    def click_outbound(self, link, site, url_pattern=None):
+    def click_outbound(self, link, site):
         """
         link is a bound callable that returns an element
 
         click a link that points to a different site and return a new page object
         """
         link().click()
-        return site.newpage()
+        return site.newpage(self)
 
 
 
@@ -109,7 +137,14 @@ class PageObject:
         testcase is the testcast we're calling from
         element is a bound method that should return an element
         """
+
+        print "in assert_element"
+
+        the_element = element()
+        print the_element
         testcase.assertTrue(element() != None)
+
+        print "leaving assert element"
 
     def page_source(self):
         return self.driver.page_source
@@ -127,22 +162,32 @@ class SiteObject:
         site_class = self.__class__
 
         #Loop through pages in site
+
         for page_class in site_class.pages:
 
             #Construct a new page for each page class
-            newpage = page_class(oldpage.driver, self)
+            newpage_candidate = page_class(oldpage.driver, self)
+            if newpage_candidate.__class__.url != None:
 
-            #If the url of this new page corresponds to the url
-            #of the driver of the old page return the new page object
-            #this will be the case when we have clicked a link on the old page pointing
-            #to the url of the new page
-            if strip_trailing_slash(newpage.full_url()) == strip_trailing_slash(url_of_page_we_just_navigated_to):
-                return newpage
-
+                #If the url of this new page corresponds to the url
+                #of the driver of the old page return the new page object
+                #this will be the case when we have clicked a link on the old page pointing
+                #to the url of the new page
+                if strip_trailing_slash(newpage_candidate.full_url) == strip_trailing_slash(url_of_page_we_just_navigated_to):
+                    return newpage_candidate
         #Loop through the pages in the site again, this time looking for pattern matches
+        for page_class in site_class.pages:
+            newpage_candidate = page_class(oldpage.driver, self)
+            if newpage_candidate.__class__.url_pattern:
+                print newpage_candidate.__class__.url_pattern
 
+                compiled_regexp = re.compile(newpage_candidate.__class__.url_pattern)
+                match = compiled_regexp.search(url_of_page_we_just_navigated_to)
+                if match:
+                    newpage_candidate.full_url = url_of_page_we_just_navigated_to
+                    return newpage_candidate
 
         #if we get here our tests are broken
         #TODO make a custom exception here and raise it
 
-        raise PageNotFoundException({"message" : "The requested page [{0}]".format(newpage.full_url())})
+        raise PageNotFoundException({"message": "The requested page we and url of [{0}] could not be found within the supplied site object".format(url_of_page_we_just_navigated_to)})
